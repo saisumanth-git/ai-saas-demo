f5c6a53f623982f5f23cd04a5cdc5e08d52489ca
saisumanth-git <j.saisumanth@gmail.com>
2026-07-24 00:43:26 +0530

    Initialize Flask app with a home route

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,14 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>AI SaaS Demo Backend</h1>
+    <h3>Application is Running Successfully!</h3>
+    <p>Welcome to the AI SaaS Demo deployed using AWS Elastic Beanstalk.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
